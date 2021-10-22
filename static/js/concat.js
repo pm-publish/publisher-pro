d182070cb4c79e55c80e79a67a1aa8016d5c8456
insphere_mayank <insphere.mayank@gmail.com>
2021-10-22 12:56:13 +0530

    cache busting

--- static/js/concat.js
+++ static/js/concat.js
@@ -77,14 +77,14 @@ document.addEventListener("DOMContentLoaded", function (event) {
         $('#counter').html(item + " of " + items)
     }
 
-    $('.c-article__container figure').each(function () {
-        var figureStyle = $(this).attr('style') !== undefined;
-        var figureClassLeft = $(this).hasClass('alignleft');
-        var figureClassRight = $(this).hasClass('alignright');
-        if ((figureStyle) || (figureClassLeft) || (figureClassRight)) {
-            $(this).after('<div class="c-clearfix"></div>');
-        }
-    });
+    // $('.c-article__container figure').each(function () {
+    //     var figureStyle = $(this).attr('style') !== undefined;
+    //     var figureClassLeft = $(this).hasClass('alignleft');
+    //     var figureClassRight = $(this).hasClass('alignright');
+    //     if ((figureStyle) || (figureClassLeft) || (figureClassRight)) {
+    //         $(this).after('<div class="c-clearfix"></div>');
+    //     }
+    // });
 
     //sidebar function
 
