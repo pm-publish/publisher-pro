80526b28e93606379c030cdb47e781af545d0f8f
Lee <Leeneenan@gmail.com>
2021-12-10 16:03:29 +1100

    fix bot filter

--- static/js/signup.js
+++ static/js/signup.js
@@ -101,8 +101,7 @@ SubscribeForm.prototype.submit = function(event)
     if (!validated) return;
 
     if (self.botTimer < 5 || $('#email-confirm').val() !== "") {
-        console.log('bjot');
-        // window.location.href = location.origin + "/auth/thank-you";
+        window.location.href = location.origin + "/auth/thank-you";
     }
 
     var signup = $('#signup').val();
@@ -110,7 +109,6 @@ SubscribeForm.prototype.submit = function(event)
     var submitResponse= function(r) {
         // console.log(r);
         if (r.success == 1) {
-            
                 window.location.href = location.origin + '/auth/thank-you';
 
         } else {
